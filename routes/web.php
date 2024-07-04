821fd04f53b2cb16daca9882b70f24af1ff75d9a
traldo44 <aydingunesmert@gmail.com>
2024-07-04 15:49:36 +0300

    9. video bitti admin login yapıldı

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ Route::get('/home', function () {
 Route::get('/home',[HomeController::class, 'index'])->name('home');
 Route::get('/aboutus',[HomeController::class, 'aboutus'])->name('aboutus');
 Route::get('/test/{id}/{name}',[HomeController::class, 'test'])->name('test');
-Route::get('/admin',[App\Http\Controllers\Admin\HomeController::class, 'index'])->name('adminhome');
+Route::get('/admin/login',[HomeController::class, 'login'])->name('adminlogin');
+Route::get('/admin',[App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin');
+Route::post('/admin/logincheck',[HomeController::class, 'logincheck'])->name('admin_logincheck');
+Route::get('/admin/loginout',[HomeController::class, 'loginout'])->name('admin_logout');
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
